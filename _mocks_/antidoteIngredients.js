fa5fa44b6fedfb33946e4cfc7f3162a50aa70ccd
llabaka <lander.labaka@ikasle.aeg.eus>
2025-01-14 11:31:46 +0100

    [MAIN][ADD] Antidote tests

--- _mocks_/antidoteIngredients.js
+++ _mocks_/antidoteIngredients.js
@@ -1,25 +1,47 @@
 const antidoteIngredient = [
 	{
-		name: "Giant's Tear",
-		description: "A tear known for its ability to enhance strength and fortitude.",
-		value: 250,
+		_id: "6702b44f76863c206a48ccdf",
+		name: "Healing Ointment",
+		description: "A soothing ointment that effectively restores hit points.",
+		value: 65,
 		effects: [
-			"greater_restore_strength"
+			"restore_hit_points"
 		],
-		image: "/images/equipment/ingredients/restore_19.png",
+		image: "/images/equipment/ingredients/restore_10.png",
 		type: "ingredient"
 	},
 	{
-        name: "Tranquility Flower",
-        description: "A tranquil flower that helps to alleviate insanity and calm the mind.",
-        value: 5,
-        effects: [
-          "least_restore_insanity"
-        ],
-        image: "/images/equipment/ingredients/restore_12.png",
-        type: "ingredient"
-    },
-
+		_id: "6702b44f76863c206a48cce1",
+		name: "Tranquility Flower",
+		description: "A tranquil flower that helps to alleviate insanity and calm the mind.",
+		value: 5,
+		effects: [
+			"least_restore_insanity"
+		],
+		image: "/images/equipment/ingredients/restore_12.png",
+		type: "ingredient"
+	},
+	{
+		_id: "6702b44f76863c206a48cce7",
+		name: "Silver Petal",
+		description: "A delicate petal that enhances charisma, making interactions smoother.",
+		value: 30,
+		effects: [
+			"lesser_restore_charisma"
+		],
+		image: "/images/equipment/ingredients/restore_18.png",
+		type: "ingredient"
+	},
+	{
+		_id: "6702b44f76863c206a48ccd9",
+		name: "Fortitude Root",
+		description: "A potent root that strengthens the constitution and fortifies the body.",
+		value: 270,
+		effects: [
+			"greater_restore_constitution"
+		],
+		image: "/images/equipment/ingredients/restore_4.png",
+		type: "ingredient"
+	},
 ]
-
 module.exports = antidoteIngredient;
--- test.js
+++ test.js
@@ -3,6 +3,8 @@ const Cauldron = require("./cauldron")
 const Ingredients = require("./ingredients")
 const Curses = require("./curses")
 
+
+// TEST ANTIDOTE
 describe('Cuando todos los ingredientes llevan el efecto Restore', () => {
 
     describe('Si los ingredientes contienen los efectos necesarios para combatir una enfermedad concreta se creeara el antidoto asociado a la enfermedad', () => {
@@ -11,6 +13,8 @@ describe('Cuando todos los ingredientes llevan el efecto Restore', () => {
             // Arrange
             const ingredient1 = restoreIngredients[0];
             const ingredient2 = restoreIngredients[1];
+            const ingredient3 = restoreIngredients[2];
+            const ingredient4 = restoreIngredients[3];
 
             const fakeIngredients = require('./_mocks_/fake-ingredients.json');
             const fakeCurses = require('./_mocks_/fake-curses.json');
@@ -18,17 +22,72 @@ describe('Cuando todos los ingredientes llevan el efecto Restore', () => {
             const ingredients = Ingredients.load(fakeIngredients).ingredients;
             const curses = Curses.load(fakeCurses).curses;
 
-            const ingredientsArray = [ingredient1, ingredient2];
+            const ingredientsArray = [ingredient1, ingredient2, ingredient3, ingredient4];
 
-            console.log(ingredientsArray);
-            
+            console.log("ARRAY DE INGREDIENTES 1 Y 2");
+
+            console.log(ingredientsArray[0].effects);
+            console.log(ingredientsArray[1].effects);
+            console.log(ingredientsArray[2].effects);
+            console.log(ingredientsArray[3].effects);
 
             const cauldron = new Cauldron(ingredients, curses);
 
             // Act
             const potion = cauldron.createPotion(ingredientsArray);
 
+            console.log("POTION CREATED IN TEST");
+            console.log(potion);
+
+            // Assert
+            expect(potion).toBeDefined();
+            expect(potion.name).toBe('Antidote of Shadowblight');
+            expect(potion.effects).toContain('Restore');
 
         });
+        it('Los atributos tendran el valor que aparece en la enfermedad pero cambiado de signo o, en su defecto el rango de valores que se muestra en la tabla de creacion de pociones', () => {
+
+        })
     });
 });
+
+describe('Si alguno de los ingredientes no tiene el nombre "Restore"', () => {
+    it('No podemos crear un antidoto. El nombre de la poción creada no llevará la palabra "Antidote"', () => {
+
+        // Arrange
+        const ingredient1 = restoreIngredients[0];
+        const ingredient2 = restoreIngredients[1];
+        const ingredient3 = restoreIngredients[2];
+        const ingredient4 = restoreIngredients[3];
+
+        const fakeIngredients = require('./_mocks_/fake-ingredients.json');
+        const fakeCurses = require('./_mocks_/fake-curses.json');
+
+        const ingredients = Ingredients.load(fakeIngredients).ingredients;
+        const curses = Curses.load(fakeCurses).curses;
+
+        const ingredientsArray = [ingredient1, ingredient2, ingredient3, ingredient4];
+
+        console.log("ARRAY DE INGREDIENTES 1 Y 2");
+        console.log(ingredientsArray[0].effects);
+        console.log(ingredientsArray[1].effects);
+        console.log(ingredientsArray[2].effects);
+        console.log(ingredientsArray[3].effects);
+
+        const cauldron = new Cauldron(ingredients, curses);
+
+        // Act
+        const potion = cauldron.createPotion(ingredientsArray);
+
+        console.log("POTION CREATED IN TEST");
+        console.log(potion);
+
+        // Assert
+        expect(potion).toBeDefined();
+
+        expect(potion.name).not.toContain('Antidote');
+    });
+});
+
+// POISON
+
